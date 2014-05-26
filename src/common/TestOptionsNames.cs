416ed4ec4d8479e969b216209e8375d06d34df37
Oren Novotny <oren@novotny.org>
2014-05-26 13:10:29 -0400

    Initial check-in of Xamarin runner apps

---
+++ src/common/TestOptionsNames.cs
@@ -0,0 +1,13 @@
+internal static class TestOptionsNames
+{
+    internal static class Discovery
+    {
+    }
+
+    internal static class Execution
+    {
+        public static readonly string SynchronousMessageReporting = "xunit.SynchronousMessageReporting";
+        public static readonly string DisableParallelization = "xunit.DisableParallelization";
+        public static readonly string MaxParallelThreads = "xunit.MaxParallelThreads";
+    }
+}
