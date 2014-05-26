434a457463362e734ad228c3e280482e21bdcac0
Oren Novotny <oren@novotny.org>
2014-05-26 16:25:20 -0400

    Prep build script

---
+++ build.ps1
@@ -0,0 +1,25 @@
+param(
+    [string]$target = "Test",
+    [string]$verbosity = "minimal",
+    [int]$maxCpuCount = 0
+)
+
+# Kill all MSBUILD.EXE processes because they could very likely have a lock against our
+# MSBuild runner from when we last ran unit tests.
+get-process -name "msbuild" -ea SilentlyContinue | %{ stop-process $_.ID -force }
+
+$msbuilds = @(get-command msbuild -ea SilentlyContinue)
+if ($msbuilds.Count -eq 0) {
+    $msbuild = join-path $env:windir "Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+} else {
+    $msbuild = $msbuilds[0].Definition
+}
+
+if ($maxCpuCount -lt 1) {
+    $maxCpuCountText = $Env:MSBuildProcessorCount
+} else {
+    $maxCpuCountText = ":$maxCpuCount"
+}
+
+$allArgs = @("xunit.xamarin.proj", "/m$maxCpuCountText", "/nologo", "/verbosity:$verbosity", "/t:$target", "/property:RequestedVerbosity=$verbosity", $args)
+& $msbuild $allArgs
