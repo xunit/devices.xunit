c31bc105f473af0071103dfe945cdb19fd1ce759
Oren Novotny <oren@novotny.org>
2014-11-02 10:37:35 -0800

    Validate WP8 tests with xunit. need to refactor code to make better

---
+++ src/xunit.runner.wp8/MainPageControl.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Xamarin.Forms;
+using Xunit.Runners;
+
+namespace xunit.runner.wp8
+{
+    public partial class MainPageControl : UserControl
+    {
+
+        FormsRunner runner;
+
+        private Assembly executionAssembly;
+        private readonly List<Assembly> testAssemblies = new List<Assembly>();
+
+        public bool TerminateAfterExecution { get; set; }
+        public TextWriter Writer { get; set; }
+        public bool AutoStart { get; set; }
+        public bool Initialized { get; private set; }
+        public MainPageControl()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+        }
+
+
+        public void AddExecutionAssembly(Assembly assembly)
+        {
+            if (assembly == null) throw new ArgumentNullException("assembly");
+
+            if (!Initialized)
+            {
+                executionAssembly = assembly;
+            }
+        }
+
+        public void AddTestAssembly(Assembly assembly)
+        {
+            if (assembly == null) throw new ArgumentNullException("assembly");
+            if (!Initialized)
+            {
+                testAssemblies.Add(assembly);
+            }
+        }
+
+        public void FinishInit(PhoneApplicationPage page)
+        {
+            runner = new FormsRunner(executionAssembly, testAssemblies)
+            {
+                TerminateAfterExecution = TerminateAfterExecution,
+                Writer = Writer,
+                AutoStart = AutoStart,
+            };
+
+            Content = runner.GetMainPage().ConvertPageToUIElement(page);
+
+            Initialized = true;
+        }
+    }
+}
--- test/test.xunit.runner.wp8/MainPage.xaml.cs
+++ test/test.xunit.runner.wp8/MainPage.xaml.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using test.xunit.runner.wp8.Resources;
+using Xamarin.Forms;
+using xunit.runner.wp8;
+using Xunit.Sdk;
 
 namespace test.xunit.runner.wp8
 {
@@ -20,6 +24,24 @@ namespace test.xunit.runner.wp8
 
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
+
+            var mainPage = new MainPageControl();
+
+
+     //       mainPage.AddExecutionAssembly(typeof(ExtensibilityPointFactory).Assembly);
+
+
+            // tests can be inside the main assembly
+            mainPage.AddTestAssembly(Assembly.GetExecutingAssembly());
+            // otherwise you need to ensure that the test assemblies will 
+            // become part of the app bundle
+          //  mainPage.AddTestAssembly(typeof(PortableTests).Assembly);
+
+
+            mainPage.FinishInit(this);
+           Content = mainPage;
+
+
         }
 
         // Sample code for building a localized ApplicationBar
