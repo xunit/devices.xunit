50dbaa868733b5a5f571fc140934966e7e1fe57d
Kent Boogaart <kent.boogaart@gmail.com>
2015-02-02 15:28:41 +1030

    Fixes #11.

--- src/xunit.runner.xamarin/ViewModels/TestAssemblyViewModel.cs
+++ src/xunit.runner.xamarin/ViewModels/TestAssemblyViewModel.cs
@@ -146,7 +146,7 @@ namespace Xunit.Runners.ViewModels
             }
 
             var pattern = query.Item1;
-            return string.IsNullOrWhiteSpace(pattern) || test.DisplayName.IndexOf(pattern.Trim(), StringComparison.OrdinalIgnoreCase) >= 0;
+            return string.IsNullOrWhiteSpace(pattern) || test.UniqueName.IndexOf(pattern.Trim(), StringComparison.OrdinalIgnoreCase) >= 0;
         }
 
         public string SearchQuery
