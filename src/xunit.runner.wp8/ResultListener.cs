c19299311991734a0fefc498ebffa6e1184126b8
Oren Novotny <oren@novotny.org>
2015-09-03 13:21:52 -0400

    protect against race conditions

--- src/xunit.runner.wp8/ResultListener.cs
+++ src/xunit.runner.wp8/ResultListener.cs
@@ -16,7 +16,7 @@ namespace Xunit.Runners
         int failed;
         int skipped;
         int passed;
-
+        readonly object lockOjb = new object();
         public ResultListener(TextWriter writer)
         {
             this.writer = writer;
@@ -24,62 +24,68 @@ namespace Xunit.Runners
 
         public void RecordResult(TestResultViewModel result)
         {
-            if (writer == null)
-                return;
+            lock (lockOjb)
+            {
+                if (writer == null)
+                    return;
 
-            if (result.TestCase.Result == TestState.Passed)
-            {
-                writer.Write("\t[PASS] ");
-                passed++;
-            }
-            else if (result.TestCase.Result == TestState.Skipped)
-            {
-                writer.Write("\t[SKIPPED] ");
-                skipped++;
-            }
-            else if (result.TestCase.Result == TestState.Failed)
-            {
-                writer.Write("\t[FAIL] ");
-                failed++;
-            }
-            else
-            {
-                writer.Write("\t[INFO] ");
-            }
-            writer.Write(result.TestCase.DisplayName);
+                if (result.TestCase.Result == TestState.Passed)
+                {
+                    writer.Write("\t[PASS] ");
+                    passed++;
+                }
+                else if (result.TestCase.Result == TestState.Skipped)
+                {
+                    writer.Write("\t[SKIPPED] ");
+                    skipped++;
+                }
+                else if (result.TestCase.Result == TestState.Failed)
+                {
+                    writer.Write("\t[FAIL] ");
+                    failed++;
+                }
+                else
+                {
+                    writer.Write("\t[INFO] ");
+                }
+                writer.Write(result.TestCase.DisplayName);
 
-            var message = result.ErrorMessage;
-            if (!string.IsNullOrEmpty(message))
-            {
-                writer.Write(" : {0}", message.Replace("\r\n", "\\r\\n"));
-            }
-            writer.WriteLine();
+                var message = result.ErrorMessage;
+                if (!string.IsNullOrEmpty(message))
+                {
+                    writer.Write(" : {0}", message.Replace("\r\n", "\\r\\n"));
+                }
+                writer.WriteLine();
 
-            var stacktrace = result.ErrorStackTrace;
-            if (!string.IsNullOrEmpty(result.ErrorStackTrace))
-            {
-                var lines = stacktrace.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-                foreach (var line in lines)
-                    writer.WriteLine("\t\t{0}", line);
+                var stacktrace = result.ErrorStackTrace;
+                if (!string.IsNullOrEmpty(result.ErrorStackTrace))
+                {
+                    var lines = stacktrace.Split(new char[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var line in lines)
+                        writer.WriteLine("\t\t{0}", line);
+                }
             }
         }
 
         public Task<bool> OpenChannel(string message = null)
         {
-            var r = OpenWriter(message);
-
-            return Task.FromResult(r);
+            lock (lockOjb)
+            {
+                var r = OpenWriter(message);
+                return Task.FromResult(r);
+            }
         }
 
         public Task CloseChannel()
         {
-            var total = passed + failed; // ignored are *not* run
-            writer.WriteLine("Tests run: {0} Passed: {1} Failed: {2} Skipped: {3}", total, passed, failed, skipped);
-
-            writer.Dispose();
-            writer = null;
-
-            return Task.FromResult(true);
+            lock (lockOjb)
+            {
+                var total = passed + failed; // ignored are *not* run
+                writer.WriteLine("Tests run: {0} Passed: {1} Failed: {2} Skipped: {3}", total, passed, failed, skipped);
+                writer.Dispose();
+                writer = null;
+                return Task.FromResult(true);
+            }
         }
 
 
