b3e46dc782434877b80c0855f7c4cb7a62a70edb
Oren Novotny <oren@novotny.org>
2015-11-29 14:35:19 -0500

    Remove UIFact/UITheory from devices. Moved to xunit.extensions.uitest

--- test/test.xunit.runner.uwp/UWPRunnerTests.cs
+++ test/test.xunit.runner.uwp/UWPRunnerTests.cs
@@ -31,37 +31,6 @@ public class UWPRunnerTests
         Assert.True(true);
     }
 
-    [UIFact]
-    public async void DoSomethingOnUIThread()
-    {
-        var ele = new SearchBox();
-        ele.Visibility = Visibility.Collapsed;
-
-        ele.QueryText = "foo";
-
-        Assert.Equal("foo", ele.QueryText);
-
-        await Task.Delay(20);
-
-        ele.QueryText = "bar";
-        Assert.Equal("bar", ele.QueryText);
-    }
-
-    [UITheory]
-    [InlineData("foo")]
-    [InlineData("FooBar")]
-    [InlineData("Bar")]
-    public void UITheory(string value)
-    {
-        var ele = new SearchBox();
-        ele.Visibility = Visibility.Collapsed;
-        ele.QueryText = value;
-
-
-        Assert.Equal(3, ele.QueryText.Length);
-    }
-    
-
     [Fact]
     public async void TestAsyncVoid()
     {
