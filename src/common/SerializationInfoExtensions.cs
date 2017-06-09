dc2d24710be8aec5f7dbe37c9a3c83987150bbd0
Oren Novotny <oren@novotny.org>
2017-06-09 15:25:27 -0400

    remove old files

--- src/common/SerializationInfoExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.Serialization;
-
-internal static class SerializationInfoExtensions
-{
-    public static T GetValue<T>(this SerializationInfo info, string name)
-    {
-        return (T)info.GetValue(name, typeof(T));
-    }
-}
