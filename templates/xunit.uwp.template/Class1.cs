5fe658b345804c297eec4c9f4ccd25a8a2daba11
Oren Novotny <oren@novotny.org>
2018-01-08 23:14:08 -0500

    Add UWP project template

---
+++ templates/xunit.uwp.template/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace $safeprojectname$
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
