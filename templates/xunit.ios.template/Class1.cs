051f76a051c16f050a2391123cd31e72a375db2f
Oren Novotny <oren@novotny.org>
2018-01-08 21:48:32 -0500

    Add iOS template

--- templates/xunit.ios.template/Class1.cs
+++ templates/xunit.ios.template/Class1.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
-$endif$using System.Text;
+using Xunit;
 
 namespace $safeprojectname$
 {
-	public class Class1
-{
-}
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
 }
