f932e85843e6a88ce8250eda78bffd169c7f22b3
Oren Novotny <oren@novotny.org>
2018-09-08 13:49:01 -0400

    fix sign script

--- scripts/Sign-Package.ps1
+++ scripts/Sign-Package.ps1
@@ -6,22 +6,19 @@ if([string]::IsNullOrEmpty($env:SignClientSecret)){
 	return;
 }
 
-& nuget install SignClient -Version 0.9.1 -SolutionDir "$currentDirectory\..\" -Verbosity quiet -ExcludeVersion
-# Setup Variables we need to pass into the sign client tool
+dotnet tool install --tool-path . SignClient
 
+# Setup Variables we need to pass into the sign client tool
 $appSettings = "$currentDirectory\appsettings.json"
 $filter = "$currentDirectory\filter.txt"
-
-$appPath = "$currentDirectory\..\packages\SignClient\tools\netcoreapp2.0\SignClient.dll"
 
 $nupgks = ls $Env:ArtifactDirectory\*.nupkg | Select -ExpandProperty FullName
 $vsixs = ls $Env:ArtifactDirectory\*.vsix | Select -ExpandProperty FullName
 
-
 foreach ($nupkg in $nupgks){
 	Write-Host "Submitting $nupkg for signing"
 
-	dotnet $appPath 'sign' -c $appSettings -i $nupkg -f $filter -r $env:SignClientUser -s $env:SignClientSecret -n 'xUnit.net' -d 'xUnit.net' -u 'https://github.com/xunit/devices.xunit' 
+	.\SignClient 'sign' -c $appSettings -i $nupkg -f $filter -r $env:SignClientUser -s $env:SignClientSecret -n 'xUnit.net' -d 'xUnit.net' -u 'https://github.com/xunit/devices.xunit' 
 
 	Write-Host "Finished signing $nupkg"
 }
@@ -29,7 +26,7 @@ foreach ($nupkg in $nupgks){
 foreach ($vsix in $vsixs){
 	Write-Host "Submitting $vsix for signing"
 
-	dotnet $appPath 'sign' -c $appSettings -i $vsix -f $filter -r $env:SignClientUser -s $env:SignClientSecret -n 'xUnit.net' -d 'xUnit.net' -u 'https://github.com/xunit/devices.xunit' 
+	.\SignClient 'sign' -c $appSettings -i $vsix -f $filter -r $env:SignClientUser -s $env:SignClientSecret -n 'xUnit.net' -d 'xUnit.net' -u 'https://github.com/xunit/devices.xunit' 
 
 	Write-Host "Finished signing $nupkg"
 }
