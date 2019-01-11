8b140132336b3e2cd62b7ce4e3b75de364a85e5d
Matthew Richardson <matthew@velocitysystems.co.nz>
2019-01-11 11:33:49 +0000

    Add test.xunit.runner.macOS.

---
+++ test/test.xunit.runnner.macOS/AppDelegate.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using AppKit;
+using Foundation;
+using Xunit.Runners;
+using Xunit.Sdk;
+
+namespace test.xunit.runnner.macOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from macOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : RunnerAppDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // We need this to ensure the execution assembly is part of the app bundle
+            AddExecutionAssembly(typeof(ExtensibilityPointFactory).Assembly);
+            
+
+            // tests can be inside the main assembly
+            AddTestAssembly(Assembly.GetExecutingAssembly());
+            // otherwise you need to ensure that the test assemblies will 
+            // become part of the app bundle
+            //AddTestAssembly(typeof(PortableTests).Assembly);
+
+#if false
+            // you can use the default or set your own custom writer (e.g. save to web site and tweet it ;-)
+            Writer = new TcpTextWriter ("10.0.1.2", 16384);
+            // start running the test suites as soon as the application is loaded
+            AutoStart = true;
+            // crash the application (to ensure it's ended) and return to springboard
+            TerminateAfterExecution = true;
+#endif
+            base.DidFinishLaunching(notification);
+        }
+    }
+}
---
+++ test/test.xunit.runnner.macOS/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace test.xunit.runnner.macOS
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
